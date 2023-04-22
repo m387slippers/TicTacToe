1cb4dcf8b8de8865a0e1e606303229fa599cb603
m387slippers <130423520+m387slippers@users.noreply.github.com>
2023-04-22 17:52:56 -0600

    Add files via upload
    I believe I only updated the Controller and TicTacToe classes, but I opted to update all files contained on GitHub before hopping off for the day. Updated constructor in TTTButton so displayed buttons should all have the same font size and each of them has an action listener. Currently the action listeners' only function is to println 'x' to signify that the button has been pressed.

--- Controller.java
+++ Controller.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-/** Currently, controller contains nothing.
+/** Currently, controller does not contain nothing.
  * 
  * @author zachp
  *
@@ -61,7 +61,7 @@ public class Controller {
 		TicTacToeButton r1c2 = new TicTacToeButton(1,2);
 		r1c2.setHorizontalTextPosition(SwingConstants.CENTER);
 		r1c2.setBounds(150, 117, 135, 127);
-		r1c2.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		//r1c2.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		r1c2.setText("Row 1 col 2");
 		//contentPane.add(r1c2);
 		myArray[1] = r1c2;
@@ -69,7 +69,7 @@ public class Controller {
 		TicTacToeButton r1c3 = new TicTacToeButton(1,3);
 		r1c3.setHorizontalTextPosition(SwingConstants.CENTER);
 		r1c3.setBounds(295, 117, 135, 127);
-		r1c3.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		//r1c3.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		r1c3.setText("row 1 col 3");
 		//contentPane.add(r1c3);
 		myArray[2] = r1c3;
@@ -77,7 +77,7 @@ public class Controller {
 		TicTacToeButton r2c1 = new TicTacToeButton(2,1);
 		r2c1.setHorizontalTextPosition(SwingConstants.CENTER);
 		r2c1.setBounds(5, 255, 135, 127);
-		r2c1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		//r2c1.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		r2c1.setText("row 2 col 1");
 		//contentPane.add(r2c1);
 		myArray[3] = r2c1;
@@ -86,10 +86,11 @@ public class Controller {
 		r2c2.setHorizontalTextPosition(SwingConstants.CENTER);
 		r2c2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				System.out.println("center");
 			}
 		});
 		r2c2.setBounds(150, 255, 135, 127);
-		r2c2.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		//r2c2.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		r2c2.setText("row 2 col 2");
 		//contentPane.add(r2c2);
 		myArray[4] = r2c2;
@@ -97,7 +98,7 @@ public class Controller {
 		TicTacToeButton r2c3 = new TicTacToeButton(2,3);
 		r2c3.setHorizontalTextPosition(SwingConstants.CENTER);
 		r2c3.setBounds(295, 255, 135, 127);
-		r2c3.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		//r2c3.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		r2c3.setText("row 2 col 3");
 		//contentPane.add(r2c3);
 		myArray[5] = r2c3;
@@ -105,7 +106,7 @@ public class Controller {
 		TicTacToeButton r3c1 = new TicTacToeButton(3,1);
 		r3c1.setHorizontalTextPosition(SwingConstants.CENTER);
 		r3c1.setBounds(5, 393, 135, 127);
-		r3c1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		//r3c1.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		r3c1.setText("row 3 col 1");
 		//contentPane.add(r3c1);
 		myArray[6] = r3c1;
@@ -113,7 +114,7 @@ public class Controller {
 		TicTacToeButton r3c2 = new TicTacToeButton(3,2);
 		r3c2.setHorizontalTextPosition(SwingConstants.CENTER);
 		r3c2.setBounds(150, 393, 135, 127);
-		r3c2.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		//r3c2.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		r3c2.setText("row 3 col 2");
 		//contentPane.add(r3c2);
 		myArray[7] = r3c2;	
@@ -121,7 +122,7 @@ public class Controller {
 		TicTacToeButton r3c3 = new TicTacToeButton(3,3);
 		r3c3.setHorizontalTextPosition(SwingConstants.CENTER);
 		r3c3.setBounds(295, 393, 135, 127);
-		r3c3.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		//r3c3.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		r3c3.setText("row 3 col 3");
 		//contentPane.add(r3c3);
 		myArray[8] = r3c3;
--- TicTacToeButton.java
+++ TicTacToeButton.java
@@ -3,6 +3,10 @@
  */
 package teamProject;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
 
@@ -20,6 +24,24 @@ public class TicTacToeButton extends JButton {
 	public TicTacToeButton(int rw, int col) {
 		int row = rw;
 		int column = col;
+		this.setFont(new Font("Tahoma", Font.BOLD, 99));
+		this.addActionListener((new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				/*
+				 * Things that should happen when button pressed:
+				 * 
+				 * Turn changes.
+				 * X or O appears
+				 * Win is checked. 
+				 * 
+				 * see whose turn it is, 
+				 * depending on turn, output x or o
+				 * check for win condition. 
+				 */
+				System.out.println("X");
+			}
+			}
+		));
 	}
 
 	
