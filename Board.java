52645f8b5643280d829344daddb58b1c2c719f09
CIA8 <130424290+CIA8@users.noreply.github.com>
2023-04-23 17:55:23 -0600

    Add files via upload
    This is what I have so far. Got more to do till it can somewhat work.

--- Board.java
+++ Board.java
@@ -1,4 +1,10 @@
+/**
+ * 
+ */
 package teamProject;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * @author zachp
@@ -10,7 +16,26 @@ public class Board {
 	 * 
 	 */
 	public Board() {
-		// TODO Auto-generated constructor stub
+		// Read in save state from file when first creating game board
+		
+		String[][] boardArray = new String[3][3];
+		
+		try (Scanner input = new Scanner((Readable) new File(TicTacToeBoard.csv))) {
+			if (input.hasNextLine()) {
+				System.out.println(input.nextLine());
+			}
+			while (input.hasNextLine()) {
+				String playerTurn = input.next();
+				for (int i = 1; i < 4; i++) {
+					for (int j = 1; j < 4; j++) {
+						
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File TicTacToeBoard.csv could not be found");
+		}
+	
 	}
 
 }
